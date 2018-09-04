d068a4c0b16681f656b8901a93d3f91a442002f4
Renee Sarley <reneesarley@gmail.com>
2018-09-04 10:28:58 -0700

    add method to render to dom

--- scripts.js
+++ scripts.js
@@ -36,6 +36,17 @@ const { createStore } = Redux;
 const store = createStore(reducer);
 console.log(store.getState());
 
+//RENDERING STATE IN THE DOM
+const renderlyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
 // CLICK LISTENER
 const userClick = () => {
   console.log('click');
